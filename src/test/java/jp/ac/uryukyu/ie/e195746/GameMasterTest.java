f8a57de9c969118a4c18a20fdb10c34fbb9b6a21
KohamaKaito <e195746@ie.u-ryukyu.ac.jp>
2020-01-04 23:25:51 +0900

    GameMasterクラスに追加したisSpaceメソッドが正しく動くかユニットテストを実行

---
+++ src/test/java/jp/ac/uryukyu/ie/e195746/GameMasterTest.java
@@ -0,0 +1,35 @@
+package jp.ac.uryukyu.ie.e195746;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameMasterTest {
+
+    @Test
+    void isSpace() {
+        int number = 0;
+        int alphabet = 0;
+        int actual;
+
+        Board board = new Board("⬜");
+        board.setUnit(number,alphabet,"⭕");
+        //(0,0)は空白ではく⭕が入ってる
+
+        if (board.getUnit(number,alphabet).equals(board.getUnit())) {
+            actual = 0;  //(0,0)は空白である
+            System.out.println("");
+        }
+
+        else {
+            actual = 1;  //(0,0)は空白ではない
+        }
+
+
+        int expect = 1;  //(0,0)には⭕が入ってるので期待値は１
+        
+        assertEquals(expect,actual);
+
+
+    }
+}
